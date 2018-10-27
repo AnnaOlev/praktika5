c6dde004f780beb1a5e1921ee40b623982e556b7
anna <olevsckaya@yandex.ru>
2018-10-27 20:20:13 +0300

    norm commit

---
+++ src/recurs/funcs.java
@@ -0,0 +1,26 @@
+package recurs;
+
+public class funcs {
+	public static void z1(int n) {
+		if (n>1)
+			z1(n-1);
+		for (int i = n; i > 0; i --) {
+			System.out.println(n);
+		}
+	}	
+	
+	public static void z2 (int n) {
+		if (n>1)
+			z2(n-1);
+		System.out.println(n);
+	}
+
+	
+	public static void main (String[] args) {
+		System.out.println("Решение задачи 1");
+		z1(3);
+		System.out.println("Решение задачи 2");
+		z2(5);
+	}
+
+}
